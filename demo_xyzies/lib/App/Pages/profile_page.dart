89ea91d3510d2bfc713425bf1f4747b4e7acaf84
Sergioarg <sergio_ramos53@yahoo.com>
2021-12-11 14:22:04 -0500

    Create photo with profile photo

--- demo_xyzies/lib/App/Pages/profile_page.dart
+++ demo_xyzies/lib/App/Pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:demo_xyzies/Shared/const_images.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,20 +9,29 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _BussinesPageState extends State<ProfilePage> {
-  final String _title = 'Profile - Demo XYZies';
+  final String _title = 'Profile';
+  final double coverHeight = 280;
 
   @override
   Widget build(BuildContext context) {
-    return Title(
-      title: _title,
-      color: Colors.blue, // This is required
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Profile'),
-          centerTitle: true,
-        ),
-        body: const Center(child: Text('Profiile :)', style: TextStyle(fontSize: 60))),
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          buildCoverImge()
+          ],
       ),
     );
   }
+
+  // widget buildcover image
+  Widget buildCoverImge() => Container(
+        color: Colors.grey,
+        child: Image.network(
+          techBackground,
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.cover,
+        ),
+      );
 }
---
+++ demo_xyzies/lib/Shared/const_images.dart
@@ -0,0 +1,2 @@
+// Constants of Internet Images Links
+const String techBackground = 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHw%3D&w=1000&q=80';
