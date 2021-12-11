c21e02f44cc09f5ab09fec92c5c97891d6570538
Sergioarg <sergio_ramos53@yahoo.com>
2021-12-11 16:10:02 -0500

    Refactor widgets to new file

--- demo_xyzies/lib/App/Pages/profile_page.dart
+++ demo_xyzies/lib/App/Pages/profile_page.dart
@@ -1,18 +1,17 @@
-import 'package:demo_xyzies/Shared/const_images.dart';
+import 'package:demo_xyzies/App/Widgets/widgets_profile.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  _BussinesPageState createState() => _BussinesPageState();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-class _BussinesPageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage> {
   final String _title = 'Profile';
-  final double coverHeight = 280;
-  final double profileHeight = 144;
+  // final double coverHeight = 280;
+  // final double profileHeight = 144;
 
   @override
   Widget build(BuildContext context) {
@@ -22,84 +21,4 @@ class _BussinesPageState extends State<ProfilePage> {
           children: <Widget>[buildTop(), buildContent()]),
     );
   }
-
-  // ! --------------------------------------------
-  // contruye el tope de la funcion
-  Widget buildTop() {
-    final botton = profileHeight / 2;
-    final top = coverHeight - profileHeight / 2;
-
-    return Stack(
-      clipBehavior: Clip.none,
-      alignment: Alignment.center,
-      children: [
-        Container(
-            margin: EdgeInsets.only(bottom: botton), child: buildCoverImage()),
-        Positioned(top: top, child: buildProfileImage()),
-      ],
-    );
-  }
-
-  // widget buildcover image
-  Widget buildCoverImage() => Container(
-        color: Colors.grey,
-        child: Image.network(
-          techBackground,
-          width: double.infinity,
-          height: coverHeight,
-          fit: BoxFit.cover,
-        ),
-      );
-
-  // widget buildcover image
-  Widget buildProfileImage() => CircleAvatar(
-        radius: profileHeight / 2,
-        backgroundColor: Colors.grey.shade800,
-        backgroundImage: const NetworkImage(profilePhoto),
-      );
-
-  // widget buildcover image
-  Widget buildContent() => Column(
-        children: [
-          const SizedBox(height: 8),
-          Text('Sergio Ramos',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-          const SizedBox(height: 8),
-          Text('Full Stack Developer',
-              style: TextStyle(fontSize: 20, color: Colors.black38)),
-          const SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              buildSocialIcon(FontAwesomeIcons.slack),
-              const SizedBox(width: 12),
-              buildSocialIcon(FontAwesomeIcons.github),
-              const SizedBox(width: 12),
-              buildSocialIcon(FontAwesomeIcons.linkedin),
-            ],
-          ),
-            //  Text('About',
-            //  textAlign: TextAlign.justify,
-            //   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, )),
-            //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
-            //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-            //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
-            //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-            //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
-            //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-            //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
-            //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-            //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
-            //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-            //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
-            //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-        ],
-      );
-  // widget buildcover image
-  Widget buildSocialIcon(IconData icon) => CircleAvatar(
-        radius: 25,
-        child: Center(child: Icon(icon, size: 32)),
-      );
-
-  // ! --------------------------------------------
 }
--- demo_xyzies/lib/App/Widgets/widgets_profile.dart
+++ demo_xyzies/lib/App/Widgets/widgets_profile.dart
@@ -1,2 +1,90 @@
+import 'package:demo_xyzies/Shared/const_images.dart';
 import 'package:flutter/material.dart';
- // widget buildcover image
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const double coverHeight = 280;
+const double profileHeight = 144;
+
+// buildTop - build the top of the images
+Widget buildTop() {
+  const botton = profileHeight / 2;
+  const top = coverHeight - profileHeight / 2;
+
+  return Stack(
+    clipBehavior: Clip.none,
+    alignment: Alignment.center,
+    children: [
+      Container(
+          margin: const EdgeInsets.only(bottom: botton),
+          child: buildCoverImage()),
+      Positioned(top: top, child: buildProfileImage()),
+    ],
+  );
+}
+
+// buildCoverImage - build the background image of the profile
+Widget buildCoverImage() => Container(
+      color: Colors.grey,
+      child: Image.network(
+        techBackground,
+        width: double.infinity,
+        height: coverHeight,
+        fit: BoxFit.cover,
+      ),
+    );
+
+// buildProfileImage - build the profile image of the user
+Widget buildProfileImage() => CircleAvatar(
+      radius: profileHeight / 2,
+      backgroundColor: Colors.grey.shade800,
+      backgroundImage: const NetworkImage(profilePhoto),
+    );
+
+// buildSocialIcon - build the incons of the user
+Widget buildSocialIcon(IconData icon) => CircleAvatar(
+      radius: 25,
+      child: Material(
+          shape: CircleBorder(),
+          clipBehavior: Clip.hardEdge,
+          color: Colors.transparent,
+          child: InkWell(
+              onTap: () {}, child: Center(child: Icon(icon, size: 32)))),
+    );
+
+// buildContent - build the principal information of the user.
+Widget buildContent() => Column(
+      children: [
+        const SizedBox(height: 8),
+        const Text('Sergio Ramos',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        const Text('Full Stack Developer',
+            style: TextStyle(fontSize: 20, color: Colors.black38)),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildSocialIcon(FontAwesomeIcons.slack),
+            const SizedBox(width: 12),
+            buildSocialIcon(FontAwesomeIcons.github),
+            const SizedBox(width: 12),
+            buildSocialIcon(FontAwesomeIcons.linkedin),
+          ],
+        ),
+        //  Text('About',
+        //  textAlign: TextAlign.justify,
+        //   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, )),
+        //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
+        //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+        //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
+        //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+        //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
+        //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+        //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
+        //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+        //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
+        //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+        //   Text('AboutAboutAboutAboutAboutAboutAboutAbo',
+        //   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+      ],
+    );
