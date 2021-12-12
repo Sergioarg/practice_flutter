96e979e7a17363ea4bb12219e3e7bfde29d1b02f
Sergioarg <sergio_ramos53@yahoo.com>
2021-12-12 14:31:19 -0500

    Save changes

--- demo_xyzies/lib/App/Pages/todo_page.dart
+++ demo_xyzies/lib/App/Pages/todo_page.dart
@@ -1,6 +1,6 @@
 import 'package:demo_xyzies/App/Widgets/widgets_todo_page.dart';
+import 'package:demo_xyzies/main.dart';
 import 'package:flutter/material.dart';
-
 
 class TodoList extends StatefulWidget {
   @override
@@ -8,74 +8,16 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
-
-  final TextEditingController _textFieldController = TextEditingController();
-  final List<Todo> _todos = <Todo>[];
-  final String _title = 'Todo Page';
-
   @override
   Widget build(BuildContext context) {
-    // Widget template comes here
-
+    final tabs = [Container(), Container()];
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        children: _todos.map((Todo todo) {
-          return TodoItem(
-            todo: todo,
-            onTodoChanged: _handleTodoChange,
-          );
-        }).toList(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: () => _displayDialog(),
-        tooltip: 'Add item',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
-    );
-  }
-
-  void _handleTodoChange(Todo todo) {
-    setState(() {
-      todo.checked = !todo.checked;
-    });
-  }
-
-  void _addTodoItem(String name) {
-    setState(() {
-      _todos.add(Todo(name: name, checked: false));
-    });
-    _textFieldController.clear();
-  }
-
-  // Other functions
-  Future<void> _displayDialog() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Add a new todo item'),
-          content: TextField(
-            controller: _textFieldController,
-            decoration: const InputDecoration(hintText: 'Type your new todo'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Add'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _addTodoItem(_textFieldController.text);
-              },
-            ),
-          ],
-        );
-      },
+      appBar: AppBar(title: Text('Todo App'),),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white.withOpacity(0.7),
+        selectedItemColor: ,
+        items: [],),
     );
   }
 }
