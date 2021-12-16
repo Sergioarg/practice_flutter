d06988699d41736c8b39eb1cd1f8ff0005c9cd4e
Sergioarg <sergio_ramos53@yahoo.com>
2021-12-15 23:45:05 -0500

    Change photos of cars

--- demo_xyzies/lib/Shared/consts_images.dart
+++ demo_xyzies/lib/Shared/consts_images.dart
@@ -1,7 +1,13 @@
 // Constants of Internet Images Links
-const String techBackground = 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHw%3D&w=1000&q=80';
-const String profilePhoto = 'https://media-exp1.licdn.com/dms/image/C4E03AQGiu4qYo6Y8-w/profile-displayphoto-shrink_800_800/0/1590511433347?e=1644451200&v=beta&t=GXTvuzT13wVhkOUcYMTSAwo-0QeKwr59pRUdQauqYZE';
-const String classicCamaro = 'https://memolira.com/wp-content/uploads/2020/06/Chevrolet-Camaro-1969.jpg';
-const String newCamaro = 'https://i.blogs.es/3b6de1/s2/1366_2000.jpg';
-const String lamboUrus = 'https://i.blogs.es/9ea21f/lamborghini-urus-graphite-capsule-2021-02/1366_2000.jpg';
-const String bmwz4 = 'https://motor.elpais.com/wp-content/uploads/2018/08/07a4aabf-2019-bmw-z4-44-1800x728.jpg';
+const String techBackground =
+    'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHw%3D&w=1000&q=80';
+const String profilePhoto =
+    'https://media-exp1.licdn.com/dms/image/C4E03AQGiu4qYo6Y8-w/profile-displayphoto-shrink_800_800/0/1590511433347?e=1644451200&v=beta&t=GXTvuzT13wVhkOUcYMTSAwo-0QeKwr59pRUdQauqYZE';
+const String classicCamaro =
+    'https://i.blogs.es/0a0823/foose_69_camaro-01/450_1000.jpg';
+const String newCamaro =
+    'https://www.ccarprice.com/products/Chevrolet-Camaro-ZL1-Auto-2019.png';
+const String lamboUrus =
+    'https://www.autofacil.es/wp-content/uploads/2021/05/lamborghiniurus01.jpg';
+const String bmwz4 =
+    'https://http2.mlstatic.com/D_NQ_NP_745423-MCO46547082041_062021-O.jpg';
